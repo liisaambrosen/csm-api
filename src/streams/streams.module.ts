1128e0edcaea45a2593bb1667b6b877fd640c423
Liisa Ambrosen <liisa.ambrosen@accenture.com>
2024-09-09 11:44:39 -0300

    feat(create-stream): update streams module

--- src/streams/streams.module.ts
+++ src/streams/streams.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { StreamsService } from './streams.service';
 import { StreamsController } from './streams.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Stream, StreamsSchema } from './schema/streams.schema';
+import { UserModule } from '../user/user.module';
 
 @Module({
+  imports: [
+    UserModule,
+    MongooseModule.forFeature([{ name: Stream.name, schema: StreamsSchema }]),
+  ],
   providers: [StreamsService],
-  controllers: [StreamsController]
+  controllers: [StreamsController],
 })
 export class StreamsModule {}
