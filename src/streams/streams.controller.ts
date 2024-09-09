ba760e1c4d8712b6e027f39e723c560e53bd4a2f
Liisa Ambrosen <liisa.ambrosen@accenture.com>
2024-09-09 11:45:56 -0300

    feat(create-stream): implement newStream method

--- src/streams/streams.controller.ts
+++ src/streams/streams.controller.ts
@@ -1,10 +1,34 @@
-import { Controller, Post, Get, Put } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Put,
+  Res,
+  Body,
+  HttpStatus,
+} from '@nestjs/common';
+import { NewStreamDto } from './dto/new-stream.dto';
+import { StreamsService } from './streams.service';
 
 @Controller('streams')
 export class StreamsController {
+  constructor(private readonly streamsService: StreamsService) {}
+
   @Post()
-  newStream() {
-    console.log('new stream started');
+  async newStream(@Body() newStreamDto: NewStreamDto, @Res() response) {
+    try {
+      const newStream = await this.streamsService.newStream(
+        newStreamDto.userId,
+      );
+      return response.status(HttpStatus.CREATED).json({
+        message: 'Stream started successfully',
+        newStream,
+      });
+    } catch (err) {
+      return response.status(HttpStatus.BAD_REQUEST).json({
+        message: `Could not start stream: ${err.message}`,
+      });
+    }
   }
   @Get()
   listCurrentStreams() {
@@ -15,3 +39,23 @@ export class StreamsController {
     console.log('stream ended');
   }
 }
+
+// async registerUser(
+//   @Body() registerUserDto: RegisterUserDto,
+//   @Res() response,
+// ) {
+//   try {
+//     const newUser = await this.userService.register(
+//       registerUserDto.username,
+//       registerUserDto.streams_limit,
+//     );
+//     return response.status(HttpStatus.CREATED).json({
+//       message: 'User registered successfully',
+//       newUser,
+//     });
+//   } catch (err) {
+//     return response.status(HttpStatus.BAD_REQUEST).json({
+//       message: `Could not register user: ${err.message}`,
+//     });
+//   }
+// }
