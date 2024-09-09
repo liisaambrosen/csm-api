fe4bb8eb338c555c2057e3994996ec62067c91a1
Liisa Ambrosen <liisa.ambrosen@accenture.com>
2024-09-09 11:45:22 -0300

    feat(create-steeam): implement newStream and countCurrentStreams methods

--- src/streams/streams.service.ts
+++ src/streams/streams.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Stream, StreamsDocument } from './schema/streams.schema';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class StreamsService {}
+export class StreamsService {
+  constructor(
+    @InjectModel(Stream.name) private streamModel: Model<StreamsDocument>,
+    private readonly userService: UserService,
+  ) {}
+
+  async countCurrentStreamsByUserId(userId: string): Promise<number> {
+    return this.streamModel
+      .countDocuments({
+        user_id: userId,
+        end_time: null,
+      })
+      .exec();
+  }
+
+  async newStream(userId: string): Promise<Stream> {
+    const user = await this.userService.userById(userId);
+    if (!user) throw new Error('User not found');
+    const currentStreamNumber = await this.countCurrentStreamsByUserId(userId);
+    if (currentStreamNumber >= user.streams_limit) {
+      throw new Error('You have reached stream limit');
+    }
+    const stream = new this.streamModel({ user_id: userId });
+    return stream.save();
+  }
+}
