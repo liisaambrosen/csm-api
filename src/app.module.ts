0be06891a441946a10b4e04094ed83b2a862de8d
Liisa Ambrosen <liisa.ambrosen@accenture.com>
2024-09-09 09:59:30 -0300

    feat(mongoose): implement mongoose to connect to mongodb database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { StreamsModule } from './streams/streams.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, StreamsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.URL}:${process.env.PORT}/concurrent_streaming_manager`,
+    ),
+    UserModule,
+    StreamsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
